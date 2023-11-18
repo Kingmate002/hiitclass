661ba1025d1bc3dfef5ebc89ad9fb92f5357d216
Kingmate002 <kingmatedamiete@gmail.com>
2023-11-18 09:44:58 +0100

    add existing project file to git

---
+++ data/allblog.js
@@ -0,0 +1,78 @@
+export const allBlogs = [
+    {
+        id: 1,
+        title: "NSA",
+        authur: "COL. James",
+        body: "The NSA, or National Security Agency, is a United States government agency responsible for signals intelligence (SIGINT) and information assurance." +
+            "It operates as part of the U.S. Department of Defense and is tasked with monitoring and collecting foreign communications and intelligence.",
+        comments: [
+            {
+                id: 1,
+                name: "John",
+                content: "Nice work."
+            },
+            {
+                id: 2,
+                name: "Mary",
+                content: "Bravo."
+            },
+            {
+                id: 3,
+                name: "James",
+                content: "My Boy."
+            },
+            {
+                id: 4,
+                name: "Steve",
+                content: "NSA Is Good."
+            },
+            {
+                id: 5,
+                name: "John",
+                content: "God Bless You."
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: "Ranks at NSA",
+        authur: "iyin",
+        body: "Director of the National Security Agency (DIRNSA). " +
+            "Deputy Director of the National Security Agency (Deputy DIRNSA)" +
+            "Associate Deputy Director of the National Security Agency (Associate Deputy DIRNSA) " +
+            "Chief of Staff " +
+            "Senior Executive Service (SES) Members:  " +
+            "Technical Directors " +
+            "Senior Cryptologic Service (SCS) " +
+            " Cryptologic Executive Service (CES):" +
+            "Cryptologic Senior Leader (CSL) " +
+            "Military Ranks: ",
+        comments: [
+            {
+                id: 1,
+                name: "Berith",
+                content: "So many ranks."
+            },
+            {
+                id: 2,
+                name: "Christabel",
+                content: "Why the military rank."
+            },
+            {
+                id: 3,
+                name: "Joy",
+                content: "Nice Job."
+            },
+            {
+                id: 4,
+                name: "Fejiro",
+                content: "My love nice Job."
+            },
+            {
+                id: 5,
+                name: "John",
+                content: "Excellent."
+            },
+        ]
+    }
+]
